21cbe600eaf57122040cf6dc5f90061bf4a6535e
gusta <gusta.hdev@gmail.com>
2023-08-15 21:01:53 -0300

    first configuration on textInputs

--- lib/widgets/modal_expense_form_widget.dart
+++ lib/widgets/modal_expense_form_widget.dart
@@ -9,40 +9,56 @@ class ModalExpenseForm extends StatefulWidget {
 }
 
 class _ModalExpenseFormState extends State<ModalExpenseForm> {
+  var _expenseTitle = '';
+
+  void _savedExpenseTitle(String inputValue) {
+    _expenseTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(24),
-      child: Column(children: [
-        Center(child: Text("Novo Gasto")),
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        const Center(child: Text("Novo Gasto")),
         SizedBox(
           height: 400,
-          child: Column(children: [
-            TextField(
-              keyboardType: TextInputType.text,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-            ),
-            DropdownMenu(dropdownMenuEntries: [
-              DropdownMenuEntry(
-                value: Category.alimentacao,
-                label: 'Alimentação',
-              ),
-              DropdownMenuEntry(
-                value: Category.contas,
-                label: 'Contas',
-              ),
-              DropdownMenuEntry(
-                value: Category.lazer,
-                label: 'Lazer',
-              ),
-              DropdownMenuEntry(
-                value: Category.viagem,
-                label: 'Viagem',
-              ),
-            ])
-          ]),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  onSubmitted: _savedExpenseTitle,
+                  decoration: const InputDecoration(
+                      label: Text('Título'), border: OutlineInputBorder()),
+                  keyboardType: TextInputType.text,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                      label: Text('Valor'), border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+                ),
+                const DropdownMenu(
+                    label: Text('Categoria'),
+                    dropdownMenuEntries: [
+                      DropdownMenuEntry(
+                        value: Category.alimentacao,
+                        label: 'Alimentação',
+                      ),
+                      DropdownMenuEntry(
+                        value: Category.contas,
+                        label: 'Contas',
+                      ),
+                      DropdownMenuEntry(
+                        value: Category.lazer,
+                        label: 'Lazer',
+                      ),
+                      DropdownMenuEntry(
+                        value: Category.viagem,
+                        label: 'Viagem',
+                      ),
+                    ])
+              ]),
         )
       ]),
     );
