2baa80984f8ce2e942ca72fb79cffe0b88d31c74
gusta <gusta.hdev@gmail.com>
2023-08-16 21:04:26 -0300

    updates on new expense screen

--- lib/widgets/modal_expense_form_widget.dart
+++ lib/widgets/modal_expense_form_widget.dart
@@ -9,56 +9,72 @@ class ModalExpenseForm extends StatefulWidget {
 }
 
 class _ModalExpenseFormState extends State<ModalExpenseForm> {
-  var _expenseTitle = '';
+  var _expenseTitleController = TextEditingController();
 
-  void _savedExpenseTitle(String inputValue) {
-    _expenseTitle = inputValue;
+  @override
+  void dispose() {
+    _expenseTitleController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(24),
+      padding: const EdgeInsets.all(24),
       child:
           Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
         const Center(child: Text("Novo Gasto")),
         SizedBox(
           height: 400,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextField(
-                  onSubmitted: _savedExpenseTitle,
-                  decoration: const InputDecoration(
-                      label: Text('Título'), border: OutlineInputBorder()),
-                  keyboardType: TextInputType.text,
-                ),
-                const TextField(
-                  decoration: InputDecoration(
-                      label: Text('Valor'), border: OutlineInputBorder()),
-                  keyboardType: TextInputType.number,
-                ),
-                const DropdownMenu(
-                    label: Text('Categoria'),
-                    dropdownMenuEntries: [
-                      DropdownMenuEntry(
-                        value: Category.alimentacao,
-                        label: 'Alimentação',
-                      ),
-                      DropdownMenuEntry(
-                        value: Category.contas,
-                        label: 'Contas',
-                      ),
-                      DropdownMenuEntry(
-                        value: Category.lazer,
-                        label: 'Lazer',
-                      ),
-                      DropdownMenuEntry(
-                        value: Category.viagem,
-                        label: 'Viagem',
-                      ),
-                    ])
-              ]),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            TextField(
+              controller: _expenseTitleController,
+              decoration: const InputDecoration(
+                  label: Text('Título'), border: OutlineInputBorder()),
+              keyboardType: TextInputType.text,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          label: Text('Valor'), border: OutlineInputBorder()),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                  const DropdownMenu(
+                      label: Text('Categoria'),
+                      dropdownMenuEntries: [
+                        DropdownMenuEntry(
+                          value: Category.alimentacao,
+                          label: 'Alimentação',
+                        ),
+                        DropdownMenuEntry(
+                          value: Category.contas,
+                          label: 'Contas',
+                        ),
+                        DropdownMenuEntry(
+                          value: Category.lazer,
+                          label: 'Lazer',
+                        ),
+                        DropdownMenuEntry(
+                          value: Category.viagem,
+                          label: 'Viagem',
+                        ),
+                      ]),
+                ],
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Salvar Gasto'),
+            )
+          ]),
         )
       ]),
     );
